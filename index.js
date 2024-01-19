9540a02a590c5dc8626274585771552dccdfa2f9
Tari Namikpoh <tarinamikpoh@gmail.com>
2024-01-19 11:43:58 +0100

    started working on the js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+// questionBoxes.forEach(questionBox => {
+
+
+// });
+
+const faqBtn =document.querySelectorAll('.faq-btn')
+    faqBtn.forEach( btn => {
+        btn.addEventListener('click', (e)=>{
+            var questionBoxes = document.querySelectorAll('.question-box')
+            questionBoxes.forEach( box => {
+                box.classList.toggle("active")
+            });
+          
+        })
+    });
