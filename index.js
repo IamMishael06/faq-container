63d9f8587f3cc9fea691aad6e52845cbe7f9560a
Tari Namikpoh <tarinamikpoh@gmail.com>
2024-01-20 23:13:51 +0100

    stll working on faq js

--- index.js
+++ index.js
@@ -3,4 +3,12 @@
 
 
 // });
-const btns = document.querySelectorAll('.faq-btns')
+const btns = document.querySelectorAll('.faq-btn')
+const questionBox = document.querySelectorAll('.question-box')
+btns.forEach(btn => {
+    btn.addEventListener("click", (e)=>{
+        questionBox.forEach(box => {
+            box.classList.toggle('active')
+        });
+    })
+});
