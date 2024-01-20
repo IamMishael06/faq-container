4a388e429f8501b45947510e656943e4e5a3e065
Tari Namikpoh <tarinamikpoh@gmail.com>
2024-01-20 23:19:41 +0100

    last commoit tonight

--- index.js
+++ index.js
@@ -3,12 +3,17 @@
 
 
 // });
-const btns = document.querySelectorAll('.faq-btn')
+
 const questionBox = document.querySelectorAll('.question-box')
-btns.forEach(btn => {
-    btn.addEventListener("click", (e)=>{
+const plus = document.querySelectorAll('.faq-btn .bar:nth-child(2)')
         questionBox.forEach(box => {
-            box.classList.toggle('active')
+            box.addEventListener("click",(e)=>{
+                if (e.target.className.includes('faq-btn')) {
+                    const btns = box.querySelectorAll('.faq-btn')
+                    btns.forEach( btn => {
+                        box.classList.toggle('active')
+                        plus.classList.toggle('active')
+                    });
+                }
+            })
         });
-    })
-});
